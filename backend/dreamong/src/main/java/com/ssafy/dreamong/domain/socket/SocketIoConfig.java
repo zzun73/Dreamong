4ec36959cc81f0eba84638f3430cbc59724959b2
zzun73 <736300@naver.com>
2024-08-06 08:19:50 +0900

    feat: netty server configure 추가

--- backend/dreamong/src/main/java/com/ssafy/dreamong/domain/socket/SocketIoConfig.java
+++ backend/dreamong/src/main/java/com/ssafy/dreamong/domain/socket/SocketIoConfig.java
@@ -25,6 +25,14 @@ public class SocketIoConfig {
         config.setHostname(hostname);
         config.setPort(port);
         log.info("socketio server started at {}:{}", hostname, port);
+
+        // Netty 설정 추가
+        config.setBossThreads(1); // Boss 스레드는 새로운 연결을 수락하고 기존 연결의 트래픽을 분배
+        config.setWorkerThreads(4); //Worker 스레드는 실제로 데이터를 읽고 쓰는 작업을 수행
+        config.setAllowCustomRequests(true); // 커스텀 HTTP 요청을 처리 가능
+        config.setUpgradeTimeout(10000); // milliseconds
+        config.setPingInterval(25000); // milliseconds
+        config.setPingTimeout(60000); // 이 시간 내에 응답이 없으면 연결이 끊어짐
         return new SocketIOServer(config);
     }
 }
