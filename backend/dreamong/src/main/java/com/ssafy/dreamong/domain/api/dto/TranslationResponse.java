6c4edde86229eb2f3ccdbc8ac2cd14c5aa8feba1
이학현 <gkrgus008@nate.com>
2024-08-05 17:55:32 +0900

    feat: 번역 api 적용

---
+++ backend/dreamong/src/main/java/com/ssafy/dreamong/domain/api/dto/TranslationResponse.java
@@ -0,0 +1,14 @@
+package com.ssafy.dreamong.domain.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TranslationResponse {
+    private List<Translation> translations;
+}
