910c5ed1ad06f3ad05f09079cf3c56fe61d5db50
zzun73 <736300@naver.com>
2024-08-06 16:36:00 +0900

    feat: thumbnail 추가

--- backend/dreamong/src/main/java/com/ssafy/dreamong/domain/entity/room/Room.java
+++ backend/dreamong/src/main/java/com/ssafy/dreamong/domain/entity/room/Room.java
@@ -14,17 +14,20 @@ public class Room extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "dream_id")
+    @Column(name = "room_id")
     private Integer id;
-    @Column(name = "title")
+    @Column(name = "title", nullable = false)
     private String title;
-    @Column(name = "youtube_link")
+    @Column(name = "youtube_link", nullable = false)
     private String youtubeLink;
+    @Column(name = "thumbnail", nullable = false)
+    private String thumbnail;
 
     @Builder
-    public Room(Integer id, String title, String youtubeLink) {
+    public Room(Integer id, String title, String youtubeLink, String thumbnail) {
         this.id = id;
         this.title = title;
         this.youtubeLink = youtubeLink;
+        this.thumbnail = thumbnail;
     }
 }
