cce623c1aac09ef3858b9cdbab2be8ce824cf300
MINJOO-KIM <gkminju2000@gmail.com>
2024-08-14 19:21:52 +0900

    feat: 광장 전체 조회 - 토큰 만료 및 스크롤 ref 처리

--- frontend/dreamong/src/pages/SquarePage.jsx
+++ frontend/dreamong/src/pages/SquarePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroll-component';
@@ -21,38 +21,51 @@ const SquarePage = () => {
       confirmButtonText: '돌아가기',
     });
   };
+  const mainRef = useRef(null);
+  const ScrollToDiv = () => {
+    // 참조된 div가 있으면 그 위치로 스크롤 이동
+    if (mainRef.current) {
+      mainRef.current.scrollIntoView({ behavior: 'smooth' });
+      console.log(window.scrollY);
+    }
+  };
 
+  const accessToken = localStorage.getItem('accessToken');
   useEffect(() => {
+    ScrollToDiv();
+    // 토큰이 없으면 로그인페이지로 이동
+    if (!accessToken) return navigate('/login');
+    // 토큰이 존재하면 fatchDreams 실행
     fetchDreams();
   }, []);
 
   const fetchDreams = async (cursorId = null, size = 10) => {
     try {
-      const accessToken = localStorage.getItem('accessToken');
-      const response = await axios.get(
-        `${baseURL}/square/dreams`,
-        {
-          params: {
-            cursorId: cursorId,
-            size: size,
-          },
+      const response = await axios.get(`${baseURL}/square/dreams`, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+        withCredentials: true,
+        params: {
+          cursorId: cursorId,
+          size: size,
         },
-        {
-          headers: { Authorization: `Bearer ${accessToken}` },
-        },
-      );
+      });
       const newDreams = response.data.data;
-
       setDreams((prevDreams) => {
         const existingDreamIds = new Set(prevDreams.map((dream) => dream.dreamId));
         const uniqueDreams = newDreams.filter((dream) => !existingDreamIds.has(dream.dreamId));
         return [...prevDreams, ...uniqueDreams];
       });
-
       setHasMore(response.data.totalPages > page + 1);
       setPage((prevPage) => prevPage + 1);
     } catch {
-      handleError();
+      (error) => {
+        if (error.response && error.response.status === 401) {
+          navigate('/login');
+        } else {
+          console.error('오류 발생:', error);
+          navigate('/error');
+        }
+      };
     }
   };
 
@@ -61,7 +74,7 @@ const SquarePage = () => {
   };
 
   return (
-    <div className="h-screen bg-[#222222] p-6">
+    <div ref={mainRef} className="h-screen bg-[#222222] p-6">
       <InfiniteScroll
         dataLength={dreams.length}
         next={fetchDreams}
