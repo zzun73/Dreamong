f2d05ea9ce17ca553a53114f201282c70855d22e
MINJOO-KIM <gkminju2000@gmail.com>
2024-08-15 13:38:59 +0900

    feat: 통계 SkeletonUI 추가

--- frontend/dreamong/src/pages/SkeletonPage/StatisticsSkeletonPage.jsx
+++ frontend/dreamong/src/pages/SkeletonPage/StatisticsSkeletonPage.jsx
@@ -2,86 +2,84 @@ import React from 'react';
 import SkeletonBlock from './components/SkeletonBlock';
 
 const StatisticsSkeletonPage = () => {
-    const objectBlocks = [
-        { width: '1/3', height: 'full', rounded: '2xl' },
-        { width: '3/4', height: 'full', rounded: '2xl' },
-    ];
+  const objectBlocks = [
+    { width: '1/3', height: 'full', rounded: '2xl' },
+    { width: '3/4', height: 'full', rounded: '2xl' },
+  ];
 
-    const characterBlocks = [
-        { width: '4/5', height: '10', rounded: '2xl' },
-        { width: '4/5', height: '10', rounded: '2xl' },
-        { width: '4/5', height: '10', rounded: '2xl' },
-    ];
+  const characterBlocks = [
+    { width: '4/5', height: '10', rounded: '2xl' },
+    { width: '4/5', height: '10', rounded: '2xl' },
+    { width: '4/5', height: '10', rounded: '2xl' },
+  ];
 
-    const locationBlocks = [
-        { width: 'full', height: '8', rounded: '2xl' },
-        { width: 'full', height: '8', rounded: '2xl' },
-    ];
+  const locationBlocks = [
+    { width: 'full', height: '8', rounded: '2xl' },
+    { width: 'full', height: '8', rounded: '2xl' },
+  ];
 
-    const moodBlocks = [
-        { width: '1/3', height: '6', rounded: '2xl' },
-        { width: '1/3', height: '6', rounded: '2xl' },
-        { width: '1/3', height: '6', rounded: '2xl' },
-    ];
+  const moodBlocks = [
+    { width: '1/3', height: '6', rounded: '2xl' },
+    { width: '1/3', height: '6', rounded: '2xl' },
+    { width: '1/3', height: '6', rounded: '2xl' },
+  ];
 
-    const dreamTypeBlocks = [
-        { width: '1/6', height: '16', rounded: '2xl' },
-        { width: '1/6', height: '48', rounded: '2xl' },
-        { width: '1/6', height: '40', rounded: '2xl' },
-        { width: '1/6', height: '24', rounded: '2xl' },
-        { width: '1/6', height: '16', rounded: '2xl' },
-        { width: '1/6', height: '24', rounded: '2xl' },
-    ];
+  const dreamTypeBlocks = [
+    { width: '1/8', height: '16', rounded: '2xl' },
+    { width: '1/8', height: '48', rounded: '2xl' },
+    { width: '1/8', height: '36', rounded: '2xl' },
+    { width: '1/8', height: '20', rounded: '2xl' },
+    { width: '1/8', height: '36', rounded: '2xl' },
+    { width: '1/8', height: '20', rounded: '2xl' },
+  ];
 
-    return (
-        <div className="w-full h-full bg-[#3a3a3a] flex flex-col justify-center items-center">
-            <div className="w-full max-w-md h-full flex flex-col justify-start items-center p-5 bg-[#3a3a3a]">
-                <div className="flex flex-col items-end justify-start w-full h-16 gap-1">
-                    {objectBlocks.map((block, index) => (
-                        <SkeletonBlock key={index} {...block} />
-                    ))}
-                </div>
-                <div className="w-full flex-grow py-6 bg-[#3a3a3a] flex flex-col justify-end items-center gap-2.5">
-                    <div className="w-full h-64 p-6 bg-[#646464] rounded-2xl flex flex-col justify-center items-center gap-2.5 animate-pulse">
-                        {characterBlocks.map((block, index) => (
-                            <SkeletonBlock key={index} {...block} />
-                        ))}
-                    </div>
-                    <div className="w-full flex justify-start items-center gap-5 p-2.5">
-                        {[...Array(2)].map((_, i) => (
-                            <div key={i} className="w-1/2 h-40 p-5 bg-[#646464] rounded-2xl flex flex-col justify-end items-center gap-2.5 animate-pulse">
-                                {locationBlocks.map((block, index) => (
-                                    <SkeletonBlock key={index} {...block} />
-                                ))}
-                            </div>
-                        ))}
-                    </div>
-                    <div className="w-full h-48 p-6 bg-[#646464] rounded-2xl flex flex-col justify-center items-center gap-2.5 animate-pulse">
-                        <div className="flex items-center justify-center w-full gap-4 px-12">
-                            {moodBlocks.map((block, index) => (
-                                <SkeletonBlock key={index} {...block} />
-                            ))}
-                        </div>
-                        <div className="w-full flex justify-center items-center gap-4 px-2.5">
-                            {moodBlocks.map((block, index) => (
-                                <SkeletonBlock key={index} {...block} />
-                            ))}
-                        </div>
-                        <div className="flex items-center justify-center w-full gap-4 px-12">
-                            {moodBlocks.map((block, index) => (
-                                <SkeletonBlock key={index} {...block} />
-                            ))}
-                        </div>
-                    </div>
-                    <div className="w-full h-56 p-2.5 mt-4 bg-[#646464] rounded-2xl flex justify-center items-end gap-4">
-                        {dreamTypeBlocks.map((block, index) => (
-                            <SkeletonBlock key={index} {...block} />
-                        ))}
-                    </div>
-                </div>
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center bg-[#3a3a3a]">
+      <div className="flex h-full w-full max-w-md flex-col items-center justify-start bg-[#3a3a3a] p-5">
+        <div className="flex w-full flex-grow flex-col items-center justify-end gap-2.5 bg-[#3a3a3a] py-6">
+          <div className="flex h-64 w-full animate-pulse flex-col items-center justify-center gap-2.5 rounded-2xl bg-[#646464] p-6">
+            {characterBlocks.map((block, index) => (
+              <SkeletonBlock key={index} {...block} />
+            ))}
+          </div>
+          <div className="flex w-full items-center justify-start gap-5 p-2.5">
+            {[...Array(2)].map((_, i) => (
+              <div
+                key={i}
+                className="flex h-40 w-1/2 animate-pulse flex-col items-center justify-end gap-2.5 rounded-2xl bg-[#646464] p-5"
+              >
+                {locationBlocks.map((block, index) => (
+                  <SkeletonBlock key={index} {...block} />
+                ))}
+              </div>
+            ))}
+          </div>
+          <div className="flex h-48 w-full animate-pulse flex-col items-center justify-center gap-2.5 rounded-2xl bg-[#646464] p-6">
+            <div className="flex w-full items-center justify-center gap-4 px-12">
+              {moodBlocks.map((block, index) => (
+                <SkeletonBlock key={index} {...block} />
+              ))}
             </div>
+            <div className="flex w-full items-center justify-center gap-4 px-2.5">
+              {moodBlocks.map((block, index) => (
+                <SkeletonBlock key={index} {...block} />
+              ))}
+            </div>
+            <div className="flex w-full items-center justify-center gap-4 px-12">
+              {moodBlocks.map((block, index) => (
+                <SkeletonBlock key={index} {...block} />
+              ))}
+            </div>
+          </div>
+          <div className="mt-4 grid h-56 w-full grid-cols-6 items-end justify-center gap-4 rounded-2xl bg-[#646464] p-2.5">
+            {dreamTypeBlocks.map((block, index) => (
+              <SkeletonBlock key={index} {...block} />
+            ))}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default StatisticsSkeletonPage;
