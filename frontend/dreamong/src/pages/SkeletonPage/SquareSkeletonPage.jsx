bffa9498009439592f56fcb9c96a7c4d5835a23d
MINJOO-KIM <gkminju2000@gmail.com>
2024-08-15 15:48:09 +0900

    feat: 꿈광장 SkeletonUI 반영

--- frontend/dreamong/src/pages/SkeletonPage/SquareSkeletonPage.jsx
+++ frontend/dreamong/src/pages/SkeletonPage/SquareSkeletonPage.jsx
@@ -1,11 +1,8 @@
 const DreamSquareSkeletonPage = () => {
   return (
-    <div className="flex flex-wrap justify-around p-6 relative bg-[rgb(34,34,34)]">
+    <div className="relative flex flex-wrap justify-around bg-[rgb(34,34,34)] p-6">
       {Array.from({ length: 12 }).map((_, index) => (
-        <div
-          key={index}
-          className="w-40 h-40 mb-2 bg-gray-500 animate-pulse rounded-[30px]"
-        ></div>
+        <div key={index} className="mb-2 h-40 w-40 animate-pulse rounded-[30px] bg-gray-500"></div>
       ))}
     </div>
   );
--- frontend/dreamong/src/pages/SquarePage.jsx
+++ frontend/dreamong/src/pages/SquarePage.jsx
@@ -5,12 +5,14 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import { useRecoilValue } from 'recoil';
 import Swal from 'sweetalert2';
 import { baseURLState } from '../recoil/atoms';
+import SquareSkeletonPage from './SkeletonPage/SquareSkeletonPage'; // SquareSkeletonPage 컴포넌트 import
 
 const SquarePage = () => {
   const navigate = useNavigate();
   const [dreams, setDreams] = useState([]);
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(true); // 로딩 상태 추가
   const baseURL = useRecoilValue(baseURLState);
 
   const handleError = () => {
@@ -23,7 +25,6 @@ const SquarePage = () => {
   };
   const mainRef = useRef(null);
   const ScrollToDiv = () => {
-    // 참조된 div가 있으면 그 위치로 스크롤 이동
     if (mainRef.current) {
       mainRef.current.scrollIntoView({ behavior: 'smooth' });
       console.log(window.scrollY);
@@ -33,10 +34,15 @@ const SquarePage = () => {
   const accessToken = localStorage.getItem('accessToken');
   useEffect(() => {
     ScrollToDiv();
-    // 토큰이 없으면 로그인페이지로 이동
     if (!accessToken) return navigate('/login');
-    // 토큰이 존재하면 fatchDreams 실행
+
+    // 1초 후에 로딩 상태를 false로 변경
+    const timer = setTimeout(() => setLoading(false), 1500);
+
     fetchDreams();
+
+    // 컴포넌트 언마운트 시 타이머 정리
+    return () => clearTimeout(timer);
   }, []);
 
   const fetchDreams = async (cursorId = null, size = 10) => {
@@ -57,15 +63,13 @@ const SquarePage = () => {
       });
       setHasMore(response.data.totalPages > page + 1);
       setPage((prevPage) => prevPage + 1);
-    } catch {
-      (error) => {
-        if (error.response && error.response.status === 401) {
-          navigate('/login');
-        } else {
-          console.error('오류 발생:', error);
-          navigate('/error');
-        }
-      };
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        navigate('/login');
+      } else {
+        console.error('오류 발생:', error);
+        navigate('/error');
+      }
     }
   };
 
@@ -73,19 +77,22 @@ const SquarePage = () => {
     navigate(`/square/${dreamId}`);
   };
 
+  if (loading) {
+    return <SquareSkeletonPage />;
+  }
+
   return (
     <div ref={mainRef} className="h-screen bg-[#222222] p-6">
       <InfiniteScroll
         dataLength={dreams.length}
         next={fetchDreams}
         hasMore={hasMore}
-        loader={<h4>Loading...</h4>}
         className="flex flex-wrap justify-between"
       >
         {dreams.map((dream) => (
           <div
             key={dream.dreamId}
-            className="mb-4 h-40 w-40 cursor-pointer rounded-[30px] bg-gray-500"
+            className="mb-4 h-40 w-40 cursor-pointer rounded-[30px]"
             onClick={() => handleSquareClick(dream.dreamId)}
             style={{ backgroundImage: `url(${dream.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
           ></div>
