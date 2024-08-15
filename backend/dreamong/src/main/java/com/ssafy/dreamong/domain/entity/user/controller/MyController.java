3559d70aa4d8740af648e146baec977ad623c855
이학현 <gkrgus008@nate.com>
2024-08-16 00:29:50 +0900

    feat: User_Room_APi_적용

--- backend/dreamong/src/main/java/com/ssafy/dreamong/domain/entity/user/controller/MyController.java
+++ backend/dreamong/src/main/java/com/ssafy/dreamong/domain/entity/user/controller/MyController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "My", description = "마이 페이지 API")
 public class MyController {
 
-    @Operation(summary = "마이 엔드포인트", description = "마이 엔드포인트를 호출합니다.")
+    @Operation(summary = "마이 엔드포인트", description = "마이 페이지로 이동합니다.")
     @GetMapping("/my")
     public String myAPI() {
         log.info("{} , my Controller Connect", this);
